744fe9ba3137b2621d73d00cec79d0828b1d2d16
jenjerome08 <anggongjenjerome@gmail.com>
2021-08-24 09:53:03 +0800

    fafaf

---
+++ nurse/forms.py
@@ -0,0 +1,24 @@
+from django.forms import ModelForm
+from .models import Respondent, Vaccine
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
+		
+class RespondentForm(ModelForm):
+	class Meta:
+		model = Respondent
+		fields = '__all__'
+
+class VaccineForm(ModelForm):
+	class Meta:
+		model = Vaccine
+		fields = '__all__'
+
+
+
